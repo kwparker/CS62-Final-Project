dd3e69c34d62f306ef097f6ccd7a155e9f7b2203
“Kai <kwpb2023@mymail.pomona.edu>
2025-05-07 20:30:07 -0700

    schedule methods written out - 5/7

---
+++ src/teamsync/Schedule.java
@@ -0,0 +1,45 @@
+package teamsync;
+import java.util.ArrayList;
+
+
+public class Schedule implements ScheduleInterface {
+    // add an event to the schedule
+    public void addEvent(Event event) {
+
+    }
+
+    // remove an event from schedule
+    public Event removeEvent(Event event) {
+        return null;
+    } 
+
+    // check if schedule is empty
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // return number of events in schedule
+    public int size() {
+        return 0;
+    }
+
+    // detect conflicts within schedule 
+    public boolean detectConflict() {
+        return false;
+    }
+
+    // edit a pre-existing event in the schedule
+    public void editEvent(Event event, Event newEvent) {
+
+    }
+
+    // return the whole schedule
+    public ArrayList<Event> getSchedule() {
+        return null;
+    }
+
+    // clear the whole schedule
+    public void clearSchedule() {
+
+    }
+}
