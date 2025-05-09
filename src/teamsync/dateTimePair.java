04eb406e28459f7fb8081850c43e9176d80e9c7b
“Kai <kwpb2023@mymail.pomona.edu>
2025-05-09 15:14:23 -0700

    dateTimePair class - 5/9

--- src/teamsync/dateTimePair.java
+++ src/teamsync/dateTimePair.java
@@ -1,6 +1,7 @@
 package teamsync;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Objects;
 
 public class dateTimePair {
     
@@ -11,4 +12,52 @@ public class dateTimePair {
         this.date = date;
         this.time = time;
     }
+
+    /**
+     * @param other object to check equality with
+     * @return boolean indicating equality
+     */
+    public boolean equals(Object other) {
+        if (this == other){  // if the objects are the same
+            return true;  // return true
+        } else if (other == null) {  // if other is null
+            return false;  // return false
+        } else if (other.getClass() == this.getClass()) {  // if classes are the same
+            dateTimePair otherPair = (dateTimePair) other;  // cast other object as a dateTimePair
+            return (this.date.equals(otherPair.date) && this.time.equals(otherPair.time));  // check if date and time are same in both objects
+        }
+        return false;  // otherwise return false
+    }
+
+    /**
+     * @param other object to compare with
+     * @return int corresponding to to whether it's greater, equal to, or less than other object
+     */
+    public int compareTo(dateTimePair otherPair) {
+        if (this.equals(otherPair)) {  // if the pairs are equal return 0
+            return 0;
+        } else if (this.date.compareTo(otherPair.date) > 0) { // if this date is after other date return 1
+            return 1;
+        } else if (this.date.compareTo(otherPair.date) < 0) { // if this date is before other date return -1
+            return -1;
+        } else if (this.time.compareTo(otherPair.time) > 0) { // if this time after other time return 1
+            return 1;
+        } else { // if this time is before other time return -1
+            return -1;
+        }
+    }
+
+    /**
+     * @return hashcode of dateTimePair
+     */
+    public int hashCode() {
+        return Objects.hash(this.date, this.time);  // hash date and time
+    }
+
+
+
+    
+
+
+
 }
