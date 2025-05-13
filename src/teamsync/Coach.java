62d96f255d94831ce4d4f3c5d6d0f6cb89d791cf
“Kai <kwpb2023@mymail.pomona.edu>
2025-05-12 17:16:45 -0700

    misc change

--- src/teamsync/Coach.java
+++ src/teamsync/Coach.java
@@ -3,7 +3,6 @@ package teamsync;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Set;
 
--- src/teamsync/Team.java
+++ src/teamsync/Team.java
@@ -1,9 +1,7 @@
 package teamsync;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Set;
 
 // combine coach and their athletes into the same arrayList
