9561cdaf35526b29a4ebe8d05324772b779f1224
Biruk Abebe <birukabebe1044@gmail.com>
2025-10-08 21:57:52 -0700

    base for application

---
+++ teamSyncapp/app/src/main/java/com/teamSync/app/TeamSyncAppApplication.java
@@ -0,0 +1,13 @@
+package com.teamSync.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamSyncAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamSyncAppApplication.class, args);
+	}
+
+}
